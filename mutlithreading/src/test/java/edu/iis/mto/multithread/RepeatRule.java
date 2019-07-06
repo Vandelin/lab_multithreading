531c75c091acfb8eca9783a2bc4e3298e3bc1042
Piotr Rogowski <polishotaku@gmail.com>
2019-07-06 16:07:15 +0200

    Added RepeatStatement to complement Repeat Rule.

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RepeatRule.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RepeatRule.java
@@ -4,9 +4,28 @@ import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 public class RepeatRule implements TestRule {
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({java.lang.annotation.ElementType.METHOD})
+    public @interface Repeat {
+        int times();
+    }
+
+    private Repeat repeat;
+
     @Override
-    public Statement apply(Statement base, Description description) {
-        return null;
+    public Statement apply(Statement statement, Description description) {
+        Statement result = statement;
+        repeat = description.getAnnotation(Repeat.class);
+        if (repeat != null) {
+            int times = repeat.times();
+            result = new RepeatStatement(times, statement);
+        }
+        return result;
     }
 }
+
