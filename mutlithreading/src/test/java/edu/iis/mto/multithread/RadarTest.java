d5c36ec64b394f755d8a321a5c1afc59bb3bfa6d
Piotr Rogowski <polishotaku@gmail.com>
2019-07-06 12:28:26 +0200

    Created RepeatRule. Test red.

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -3,11 +3,16 @@ package edu.iis.mto.multithread;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import org.junit.Rule;
 import org.junit.Test;
 
 public class RadarTest {
 
+    @Rule
+    public RepeatRule reapeatRule = new RepeatRule();
+
     @Test
+    @Repeat(times = 1)
     public void launchPatriotOnceWhenNoticesAScudMissle() {
         PatriotBattery batteryMock = mock(PatriotBattery.class);
         Radar radar = new Radar(batteryMock);
